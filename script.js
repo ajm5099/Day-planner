12e0553413ef9d86957bd3f3d576c5f9a1670219
Alex Milroy <amilroy@gmail.com>
2020-09-16 17:28:12 -0700

    setup structure of work to be done

---
+++ script.js
@@ -0,0 +1,44 @@
+//================================================================================
+//Variables
+//================================================================================
+
+
+//================================================================================
+//Building the presentation of the calendar 
+//================================================================================
+//TODO: Write the code that will build the hour blocks
+
+//================================================================================
+//Content that goes inside the calendar
+//================================================================================
+
+    //TODO: Include the time in each block
+
+    //TODO: Include an input field in each block
+
+
+//================================================================================
+//Saving the input from the calendar
+//================================================================================
+
+        //TODO: Save the input from each block to memory
+
+    //TODO: Include a button for each block that will save the input from that block to memory, and that the memory persists after refresh is clicked
+
+//================================================================================
+//Connecting the calendar to a time and date finder
+//================================================================================
+
+//TODO: Connect a time and date finder to the code
+
+//================================================================================
+//Styling the date blocks
+//================================================================================
+
+    //TODO: Have the time and date finder style the hour blocks correctly
+
+        //TODO: Style past blocks in grey
+
+        //TODO: Style current block in red
+
+        //TODO: Style future blocks in green
