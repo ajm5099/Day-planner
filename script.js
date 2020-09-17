a0c8c4da3e9c1e2a1012d0a59ca6299c83f06251
Alex Milroy <amilroy@gmail.com>
2020-09-16 22:06:01 -0700

    structure laid out and hours added on screen

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 //================================================================================
 //Variables
 //================================================================================
+var container = $("container")
+var hourOfTheDay = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6"]
+var rowNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
 
 //================================================================================
 //Building the presentation of the calendar 
 //================================================================================
+
 //TODO: Write the code that will build the hour blocks
+
+for (let i = 0; i < hourOfTheDay.length; i++) {
+    //adding the hour to the div
+    var hour = $("<div>");
+    hour.addClass("row");
+    hour.attr("data-number", hourOfTheDay[i]);
+    hour.text(hourOfTheDay[i]);
+    hour.attr('id', 'myrow-' + i);
+    $("#planner").append(hour);
+    //adding the text block
+}
+
+for (let i = 0; i < rowNumber.length; i++) {
+var inputField = $("INPUT");
+inputField.addClass(textarea)
+inputField.attr("INPUT")
+$("myrow-[i]").append(inputField)
+}
 
 //================================================================================
 //Content that goes inside the calendar
