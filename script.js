aeaf0764281ba544e596763b51ada4118b33cbf0
Alex Milroy <amilroy@gmail.com>
2020-09-18 21:05:39 -0700

    got entire app working, by including all necessary elements inside of the creation loop

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 var hourOfTheDay = ["8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6"]
 var rowNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 var currentTime = moment().format('MMMM Do YYYY, h:mm a');
-var currentHour = JSON.parse(moment().format("h"));
-var amPm = (moment().format("a"))
+var currentHour = JSON.parse(moment().format("H"));
+
 
 
 
@@ -14,49 +14,47 @@ var amPm = (moment().format("a"))
 //================================================================================
 
 // Using a for loop
-for (let i = 0; i < hourOfTheDay.length; i++) {
-console.log(amPm);
-// loop over my array of hours and create a div for row, create div for hours, textarea, and a button, and give these ares content
+for (let myHour = 8; myHour < 19; myHour++) {
+    // loop over my array of hours and create a div for row, create div for hours, textarea, and a button, and give these ares content
     let rowMaker = $("<div class=\"row time-block\">");
-    let hourMaker =$("<div class=\"col-md-1 hour\">" + hourOfTheDay[i] + "</div>");
+    let hourMaker 
+        if (myHour <= 12) {
+            hourMaker = $("<div class=\"col-md-1 hour\">" + myHour + "</div>");
+        } else {
+            hourMaker = $("<div class=\"col-md-1 hour\">" + (myHour - 12) + "</div>");
+        }
     // var rowStyle = $("commentrow");
     // var rowStyle = $("textarea<");
-    
+
     //determine how to style the text blocks
     let commentMaker = $("<textarea class=\"col-md-10 textarea\">");
-    if (amPm == "am"){
-        if (currentHour < hourOfTheDay[i]) {
+        if (currentHour > myHour) {
             commentMaker.addClass('past');
-        } else if (currentHour == hourOfTheDay[i]) {
-            commentMaker.addClass('present');
-        } else if (currentHour > hourOfTheDay[i])  {
-            commentMaker.addClass('future');
-        }
-    } 
-    else  {
-        if (currentHour > hourOfTheDay[i]) {
-            commentMaker.addClass('past');
-        } else if (currentHour == hourOfTheDay[i]) {
+        } else if (currentHour == myHour) {
             commentMaker.addClass('present');
         } else {
             commentMaker.addClass('future');
         }
-    }
     // JSON.stringify(rowStyle)
     // console.log("Rowstyle = ", rowStyle)
-    
+
     // let commentMaker = $("<textarea class=\"col-md-8 textarea\">"); 
     let buttonMaker = $("<button class=\"col-md-1 saveBtn\">")
-    rowMaker.attr('id', 'myrow-' + i);
-    commentMaker.attr('name', 'commentrow-' + i); //this may not be necessary for local storage
+    rowMaker.attr('id', 'myrow-' + myHour);
+    commentMaker.attr('name', 'commentrow-' + myHour); //this may not be necessary for local storage
+    commentMaker.text(localStorage.getItem('hour-' + myHour));
     buttonMaker.text("save")
-    
+    buttonMaker.attr('name', 'hour-' + myHour);
     rowMaker.append(hourMaker, commentMaker, buttonMaker)
-    $(".container").append(rowMaker)
+    $(".container").append(rowMaker);
+    function clicked() {
+        console.log("woooo", commentMaker.val());
+    }
+    buttonMaker.click(clicked)
 
-    
 
-    
+
+
 
 }
 
@@ -69,35 +67,34 @@ $("#currentDay").append(currentTime);
 
 
 //TODO: how can I compare the hours of my rows, to the current time (use moment.js to get the current time and compare to the time that is being assigned to the row)
-    
+
 
 
 //TODO:Give the elements the classes that they need. Using an if statement we can check using moment.js to see if the hour that we are looping over is past the current hour, if so give the text area the class of past
 
 //TODO: Create a click event listener for the buttons
-var appts = ["a note"];
 
-function storeAppts() {
-    localStorage.setItem("appts", JSON.stringify(appts))
-}
 
-var apptInput = document.querySelector('[name="commentrow"]')
-document.querySelector(".container").addEventListener("click", function(event) {
-    if(event.target.matches("button")){
-    event.preventDefault();
-    var apptText = apptInput.value.trim();
-    if (apptText === "") {
-        return;
+
+var apptInput = document.querySelector('[name="commentrow-0"]')
+
+$(".container").click(function(event) {
+    if (event.target.matches("button")) {
+        event.preventDefault();
+        let button = $(event.target);
+        let key = button.attr("name");
+        let text = button.siblings("textarea").val();
+        var apptText = text.trim();
+        if (apptText === "") {
+            return;
+        }
+        localStorage.setItem(key, text)
     }
-    appts.push(apptText);
-    apptInput.value = ""
-        storeAppts();
-    }
-    
+
 })
 
-$(function() {
-    $('input[type="button"]').click(function() { alert('You clicked button with ID:' + this.name); });
+$(function () {
+    $('input[type="button"]').click(function () { alert('You clicked button with ID:' + this.name); });
 });
 
 
@@ -115,17 +112,8 @@ localStorage.setItem("test1", "my notes 2")
 
 //TODO: Retrieve the data from local storage using localStorage.getItem and show them back on the text area that they belong to (how can I know what text from local storage goes to what text area?)
 
-function init() {
-    var storedAppts = JSON.parse(localStorage.getItem("appts"))
-    if (storedAppts !== null) {
-        appts = storedAppts;
-    }
-    renderAppts() //I have to build a function to render the todos to their correct areas
-}
 
-
-
-var text1FromLocalStorage= localStorage.getItem("test") //getting the text test using the assigned key from earlier
+var text1FromLocalStorage = localStorage.getItem("test") //getting the text test using the assigned key from earlier
 $(".first").text(text1FromLocalStorage) //.first is the id for what we are trying to get the text from
 
 
